38f2e679cfb0e2df172a8587743968f269aa2993
androkurniawan <androkurniawan7@gmail.com>
2026-06-03 08:47:29 +0700

    latest version

--- Other 2.py
+++ Other 2.py
@@ -1,2 +1,42 @@
-for i in range(1,6):
-    print(f"3 x {i} = {3*i}")
+data = {
+    "M001": {
+        "nama": "Andi",
+        "prodi": "Matematika",
+        "semester": 2,
+        "ipk": 3.45
+    },
+    "M002": {
+        "nama": "Budi",
+        "prodi": "Statistika",
+        "semester": 4,
+        "ipk": 3.72
+    },
+    "M003": {
+        "nama": "Citra",
+        "prodi": "Aktuaria",
+        "semester": 2,
+        "ipk": 3.90
+    },
+    "M004": {
+        "nama": "Dewi",
+        "prodi": "Matematika",
+        "semester": 6,
+        "ipk": 3.90
+    }
+}
+
+tambahan = {"M005": {
+        "nama": "Putra",
+        "prodi": "Fisika",
+        "semester": 8,
+        "ipk": 2.8}
+    }
+
+data.update(tambahan)
+
+print(data)
+
+# data --> NIM --> nama
+
+# for item in data:
+#     print(data[item]["nama"])
--- Pure Integer Programming Example.py
+++ Pure Integer Programming Example.py
@@ -1,17 +1,20 @@
 import pulp
 
 # 1. Definisi problem (minimization)
-model = pulp.LpProblem("Pure_Integer_Programming_Example", pulp.LpMaximize)
+model = pulp.LpProblem("Pure_Integer_Programming_Example", pulp.LpMinimize)
 
 # 2. Variabel keputusan
-x1 = pulp.LpVariable("x1", lowBound=0, cat="Integer")
-x2 = pulp.LpVariable("x2", lowBound=0, cat="Integer")
+y1 = pulp.LpVariable("y1", lowBound=0, cat="Continous")
+y2 = pulp.LpVariable("y2", lowBound=0, cat="Continous")
+y3 = pulp.LpVariable("y3", lowBound=0, cat="Continous")
 
 # 3. Fungsi objektif
-model += 3*x1 + 2*x2
+model += 10*y1 + 15*y2 + 12*y3
 
 # 4. Kendala
-model += x1 + x2 <= 6
+model += 2*y1 + y2 + 2*y3 >= 4
+model += y1 + 3*y2 + 2*y3 >= 6
+model += y1 + 2*y2 + y3 >= 5
 
 # 5. Solve
 model.solve(pulp.PULP_CBC_CMD(msg=False))
@@ -19,5 +22,6 @@ model.solve(pulp.PULP_CBC_CMD(msg=False))
 # 6. Hasil
 print("Status:", pulp.LpStatus[model.status])
 print("Nilai optimum Z =", pulp.value(model.objective))
-print("x1 =", x1.value())
-print("x2 =", x2.value())
+print("y1 =", y1.value())
+print("y2 =", y2.value())
+print("y3 =", y3.value())
